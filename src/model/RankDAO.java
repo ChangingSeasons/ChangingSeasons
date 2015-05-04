14c0923a6d2ff45bd73afbad10c9e27abd91c555
Summit <sumit_hotwani@yahoo.com>
2015-05-03 22:35:39 -0400

    addReview added to Add product review
    ViewReview to see all reviews for 1 product
    
    viewCustomerSpecificReview to see all reviews by 1 customer

--- src/model/RankDAO.java
+++ src/model/RankDAO.java
@@ -5,6 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class RankDAO {
 
@@ -17,6 +20,25 @@ public class RankDAO {
 			ps.setInt(1, productID);
 			ps.setInt(2, customerID);
 			ps.setInt(3, rank);
+			ps.executeUpdate();
+			ps.close();
+		}catch(SQLException e){
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		DB_close();
+		return true;
+	}
+	
+	public static boolean addReview(int productID, int customerID, String review){
+		Connect();
+
+		try{
+			String q1 = "INSERT into productRank (productID, customerID, review)" + " values (?, ?, ?)";
+			PreparedStatement ps = cn.prepareStatement(q1);
+			ps.setInt(1, productID);
+			ps.setInt(2, customerID);
+			ps.setString(3, review);
 			ps.executeUpdate();
 			ps.close();
 		}catch(SQLException e){
@@ -66,5 +88,48 @@ public class RankDAO {
 		
 		return rank;
 	}
+	
+	public static List<String> viewReview(int productID){ // View all Reviews of a Single Product
+		Connect();
+		List<String> review = new ArrayList<String>();
+		try{
+			String q0 = "SELECT review FROM productRank WHERE productID="+productID;
+			Statement st = cn.createStatement();
+			ResultSet rs = st.executeQuery(q0);
+
+			while(rs.next())
+				review.add(rs.getString("review"));
+			
+			rs.close();
+			st.close();
+		}catch(SQLException se){
+			System.err.println(se.getMessage());
+			se.printStackTrace();
+		}
+		DB_close();
+		return review;
+	}
+	
+	public static List<String> viewCustomerSpecificReview(int customerID){ // Customer can See all products reviewed
+		List<String> reviews = new ArrayList<String>();
+		Connect();
+		
+		try{
+			String q0 = "SELECT review FROM productRank WHERE customerID="+customerID;
+			Statement st = cn.createStatement();
+			ResultSet rs = st.executeQuery(q0);
+
+			while(rs.next())
+				reviews.add(rs.getString("review"));
+			rs.close();
+			st.close();
+		}catch(SQLException se){
+			System.err.println(se.getMessage());
+			se.printStackTrace();
+		}
+		DB_close();
+		return reviews;
+	}
+
 
 }
